a24e4deb0f56d99119c97ae70e2964b51cb17ff5
Nguyen Khanh <nguyenkhanhit@live.com>
2025-04-18 16:56:22 +0700

    Add project files.

---
+++ NetGuardAPI/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Mvc;
+using NetGuardAPI.Models;
+using System.Diagnostics;
+
+namespace NetGuardAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TextPredict()
+        {
+            return View();
+        }
+        public IActionResult TrainModel()
+        {
+            SentimentEngine se  = new SentimentEngine();
+            se.TrainingModel();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult ImgPredict()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult<SentimentPrediction>> ImgPredict(IFormFile fileUpload)
+        {
+            if (fileUpload != null && fileUpload.Length > 0)
+            {
+                var fileext = Path.GetExtension(fileUpload.FileName);
+                var myUniqueFileName = Guid.NewGuid() + fileext;
+
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads", myUniqueFileName);
+                using (var stream = System.IO.File.Create(filePath))
+                {
+                    await fileUpload.CopyToAsync(stream);
+                }
+
+                var imageBytes = System.IO.File.ReadAllBytes(filePath);
+                NetGaurd4KidMLModel.ModelInput modelInput = new NetGaurd4KidMLModel.ModelInput()
+                {
+                    ImageSource = imageBytes,
+                };
+
+                //var sortedScoresWithLabel = ImgMLModel.Predict(sampleData);
+                var sortedScoresWithLabel = NetGaurd4KidMLModel.PredictAllLabels(modelInput);
+
+               
+                var maxScoreLabel = sortedScoresWithLabel.OrderByDescending(x => x.Value).FirstOrDefault();
+                var result = new PredictResult
+                {
+                    Prediction = maxScoreLabel.Key.ToString(),
+                    Probability = maxScoreLabel.Value
+                };
+
+                ViewBag.Prediction = maxScoreLabel.Key.ToString();
+                ViewBag.Probability = maxScoreLabel.Value;
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NetGuardAPI/Controllers/SEController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetGuardAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SEController : ControllerBase
+    {
+        //api/SE/Predict?text=a
+        [HttpGet("Predict")]
+        public async Task<ActionResult<SentimentPrediction>> Get(string text)
+        {
+            SentimentPrediction prediction = new SentimentPrediction();
+            SentimentEngine sentimentEngine = new SentimentEngine();
+            try
+            {
+                prediction = await Task.Run(() => sentimentEngine.Predict(text));
+            }
+            catch (FileNotFoundException fnf)
+            {
+                sentimentEngine.TrainingModel();
+                prediction = sentimentEngine.Predict(text);
+                Console.WriteLine(fnf.Message);
+            }
+            return Ok(prediction);
+        }
+        // GET api/<SEController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SEController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SEController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SEController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
